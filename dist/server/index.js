aabfefc6cf29c450d480796ce68b8fb012f3c466
Leonardo Petrucci <leonardo@argodigital.co.uk>
2020-11-12 13:57:08 +0000

    🚧 fixes

---
+++ dist/server/index.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var graphql_yoga_1 = require("graphql-yoga");
+var permissions_1 = require("../permissions");
+var schema_1 = require("../schema");
+var context_1 = require("../context");
+require('dotenv').config();
+// new GraphQLServer({
+//   schema,
+//   context: createContext,
+//   middlewares: [permissions],
+// }).start(() =>
+//   console.log(
+//     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
+//   ),
+// )
+var lambda = new graphql_yoga_1.GraphQLServerLambda({
+    schema: schema_1.schema,
+    context: context_1.createContext,
+    middlewares: [permissions_1.permissions]
+});
+exports.handler = lambda.handler;
---
+++ src/server/index.ts
@@ -0,0 +1,23 @@
+import {  GraphQLServerLambda } from 'graphql-yoga'
+import { permissions } from '../permissions'
+import { schema } from '../schema'
+import { createContext } from '../context'
+require('dotenv').config()
+
+// new GraphQLServer({
+//   schema,
+//   context: createContext,
+//   middlewares: [permissions],
+// }).start(() =>
+//   console.log(
+//     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
+//   ),
+// )
+
+const lambda = new GraphQLServerLambda({
+  schema,
+  context: createContext,
+  middlewares: [permissions],
+})
+
+exports.handler = lambda.handler
