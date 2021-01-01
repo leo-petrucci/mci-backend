e4ff860ae305c0379bcbc444a821899d5ba57aa8
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-01 23:55:16 +0000

    🔧 Updating cors config

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,19 @@ import { schema } from './schema'
 import { createContext } from './context'
 require('dotenv').config()
 
+const opts = {
+  port: 4000,
+  cors: {
+    credentials: true,
+    origin: ['http://localhost:3000'], // your frontend url.
+  },
+}
+
 export const server = new GraphQLServer({
   schema,
   context: createContext,
   middlewares: [permissions],
-}).start(() =>
+}).start(opts, () =>
   console.log(
     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
   ),
